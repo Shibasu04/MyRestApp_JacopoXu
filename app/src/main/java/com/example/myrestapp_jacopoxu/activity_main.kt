949dc5da154fc33570a2f546ea2719b42f57ea2a
Shibasu04 <xubuhan050418@gmail.com>
2025-05-30 11:44:23 +0200

    Copia progetto prof Ciuchetti

--- app/src/main/java/com/example/myrestapp_jacopoxu/activity_main.kt
+++ app/src/main/java/com/example/myrestapp_jacopoxu/activity_main.kt
@@ -1,12 +1,21 @@
 package com.example.myrestapp_jacopoxu
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class activity_main : AppCompatActivity() {
+
+    private val TAG = "MainActivity - MyRESTApp"
+    private lateinit var txtTitle : TextView
+    private lateinit var btnWebview : ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +25,13 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        txtTitle = findViewById<TextView>(R.id.idTitle)
+        btnWebview = findViewById<ImageButton>(R.id.idButton)
+
+        btnWebview.setOnClickListener(View.OnClickListener { val intent = Intent(this, WebviewActivity::class.java)
+            startActivity(intent) }
+        )
+
     }
 }
---
+++ app/src/main/java/com/example/myrestapp_jacopoxu/activity_webview.kt
@@ -0,0 +1,28 @@
+package com.example.myrestapp_jacopoxu
+
+import android.os.Bundle
+import android.webkit.WebView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class WebviewActivity : AppCompatActivity() {
+    private val TAG = "WebviewActivity - MyRESTApp"
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_webview)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        webView = findViewById<WebView>(R.id.idWebview)
+        webView.loadUrl("https://www.netflix.com/it/browse/genre/34399")
+
+    }
+}
